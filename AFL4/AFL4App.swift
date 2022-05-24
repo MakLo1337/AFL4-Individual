7812f7efd9dbbb45a0fbed305e542f345029622f
MakLo1337 <mrwinataa@gmail.com>
2022-05-24 08:11:15 +0700

    Initial Commit

---
+++ AFL4/AFL4App.swift
@@ -0,0 +1,17 @@
+//
+//  AFL4App.swift
+//  AFL4
+//
+//  Created by SIFT - Telkom DBT Air 4 on 24/05/22.
+//
+
+import SwiftUI
+
+@main
+struct AFL4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
